a0ff43bcf072a15b6078e3ff1a2fb77d7694f0ca
Andrew Robertson <andrewcrobertson@gmail.com>
2020-08-13 10:19:12 +1000

    feat: add .env to run project in dev environment

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ The Strenuous Life: Leaderboard for Class 052
 The webpage is built using a call to the The Strenuous Life API. To access the data, a secret named `TSL_COOKIE` has to be added to the repository. To generate the `TSL_COOKIE`,
 copy the **cookie** request header from any request to `https://strenuouslife.co/wp-admin/admin-ajax.php`.
 
+To run in development, create a `.env` file in the root of the project with a `TSL_COOKIE` value.
+
 ## Continuous Integration
 
 A [GitHub action](https://docs.github.com/en/actions) manages publishing to a [GitHub Pages](https://pages.github.com/) webpage.
---
+++ src/run.ts
@@ -0,0 +1,15 @@
+import * as fs from 'fs';
+import { minify } from 'html-minifier';
+import template from 'lodash/template';
+import * as path from 'path';
+import { getLeaderboard } from './getLeaderboard';
+import { indexTemplate } from './indexTemplate';
+
+export const run = async () => {
+  const cookie = process.env.TSL_COOKIE;
+  const data = await getLeaderboard(cookie);
+  const compiled = template(indexTemplate);
+  const htmlRaw = compiled({ data });
+  const minified = minify(htmlRaw, { minifyCSS: true, collapseWhitespace: true });
+  fs.writeFileSync(path.resolve(__dirname, '..', 'index.html'), minified);
+};
