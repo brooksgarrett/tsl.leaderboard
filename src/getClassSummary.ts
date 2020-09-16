50b831c142509f7db96115f6ad132454a86b6f98
Brooks Garrett <brooks@respond-software.com>
2020-09-16 11:05:19 -0400

    Set agon count properly in total passing

--- src/getClassSummary.ts
+++ src/getClassSummary.ts
@@ -34,7 +34,7 @@ export const getClassSummary = async (cookie: string, leaderboard: any) => {
     summaryStats.agonsCount = member.agons > 11 ? summaryStats.agonsCount + 1 : summaryStats.agonsCount;
     summaryStats.deedsCount = member.goodDeedPercentage >= 75.0 ? summaryStats.deedsCount + 1 : summaryStats.deedsCount;
     summaryStats.fitnessCount = member.fitnessPercentage >= 75.0 ? summaryStats.fitnessCount + 1 : summaryStats.fitnessCount;
-    if (member.badges > 0 && member.agons > 6 && member.goodDeedPercentage >= 75.0 && member.fitnessPercentage >= 75.0) summaryStats.passing++;
+    if (member.badges > 0 && member.agons > 11 && member.goodDeedPercentage >= 75.0 && member.fitnessPercentage >= 75.0) summaryStats.passing++;
   }
 
   return summaryStats;
